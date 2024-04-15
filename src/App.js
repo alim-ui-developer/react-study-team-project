c44bad920a92ee25dba3696e5fbd4012737da178
sanggyeol1 <sanggyeol1@gachon.ac.kr>
2024-04-15 09:37:12 +0900

    2024.04.15.한상결 test commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import SanggyeolTest from './component/SanggyeolTest/SanggyeolTest';
 
 function App() {
   return (
     <div className="App">
       <p>feature브랜치 테스트 한번 해봤슴다</p>
+      <SanggyeolTest/>
     </div>
   );
 }
---
+++ src/component/SanggyeolTest/SanggyeolTest.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './SanggyeolTest.style.css'
+
+const SanggyeolTest = () => {
+    return (
+        <div className='sanggyeol-test-box'>
+            <h3>
+                SanggyeolTest박스
+            </h3>
+            <p>테스트 한번 해봤습니다.</p>
+        </div>
+
+    )
+}
+
+export default SanggyeolTest
