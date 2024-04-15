58c33e2be3cfe7d307843f1c5cd86705e388a634
sanggyeol1 <sanggyeol1@gachon.ac.kr>
2024-04-15 09:49:51 +0900

    브랜치 테스트

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import SanggyeolTest from './component/SanggyeolTest/SanggyeolTest';
 function App() {
   return (
     <div className="App">
-      <p>feature브랜치 테스트 한번 해봤슴다</p>
+      <p>feature브랜치 테스트 한번 해봤슴다!!!</p>
       <SanggyeolTest/>
     </div>
   );
